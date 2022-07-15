d775a0f765f22b28abe07762e67133cd49d89a25
Rodrigo Aibar <99849205+RodriAibar@users.noreply.github.com>
2022-07-15 11:53:59 -0300

    creacion BackEnd

---
+++ Back-End/src/test/java/com/portfolio/RA/RaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.RA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
